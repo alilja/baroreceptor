cae4e6bb2addf44f6f8da33c37a66565338bde6c
Andrew Lilja (Work) <alilja@iastate.edu>
2013-10-03 11:50:00 -0500

    Figured out how to find runs of ascending and descending.

--- analysis.py
+++ analysis.py
@@ -1,5 +1,28 @@
 import csv
 
+def get_monotonic_subsequences(data, min_length=3):
+    direction = data[1] - data[0]  # determine direction of initial subsequence
+    subsequences = []
+    cur_seq = [data[0]]
+    for i in range(1, len(data)):
+        if direction > 0:
+            if data[i] >= data[i - 1]:
+                cur_seq.append((i,data[i]))
+            else:
+                subsequences.append(cur_seq)
+                cur_seq = [(i, data[i])]
+                direction = -1
+        else:
+            if data[i] <= data[i - 1]:
+                cur_seq.append((i,data[i]))
+            else:
+                subsequences.append(cur_seq)
+                cur_seq = [(i, data[i])]
+                direction = -1
+
+    subsequences.append(cur_seq)
+    return [x for x in subsequences if len(x) >= min_length]
+
 def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
     f = open(fileName,"r")
     reader = csv.reader(f)
@@ -10,44 +33,16 @@ def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
     lineNum = 0
     for line in reader:
         if(lineNum > 1): #skip the headers
-            if(line[5] != HR[-1]):
+            if(line[5] != str(HR[-1])):
                 HR.append(float(line[5]))
             if(line[4] != systolic[-1]):
-                systolic.append(float(line[2]))
+                systolic.append(float(line[4]))
 
         lineNum += 1
     f.close()
 
-    clusters = []
-    currentSysDirection = ""
-    currentHRDirection = ""
-
-    currentSysRun = [0]
-    currentHRRun = [0]
-
-    for i in range(0, len(systolic)):
-        if(i <= len(HR) - (lag + clusterWidth)):
-            if(currentSysDirection == "+" or currentSysDirection == ""):
-                if(systolic[i] >= currentSysRun[-1]):
-                    currentSysRun.append(systolic[i]) #might have to grab beginning and ending i instead to make it play nice with HR
-                    currentSysDirection = "+"
-                else: #end of run
-                    if(len(currentSysRun) >= clusterWidth):
-                        clusters.append(currentSysRun)
-                    currentSysRun = [0]
-                    currentSysDirection = ""
-
-            if(currentSysDirection == "-" or currentSysDirection == ""):
-                if(systolic[i] <= currentSysRun[-1]):
-                    currentSysRun.append(systolic[i])
-                    currentSysDirection = "-"
-                else: #end of run
-                    if(len(currentSysRun) >= clusterWidth):
-                        clusters.append(currentSysRun)
-                    currentSysRun = [0]
-                    currentSysDirection = ""
-
-    print(clusters[0:10])
+    sysRuns = get_monotonic_subsequences(systolic, 3)
+    HRRuns = get_monotonic_subsequences(HR, 3)
 
 
 
