c35e384027ab1a4a6d3f9617c04aef263431615b
Andrew Lilja <andrewlilja@gmail.com>
2013-10-03 16:01:06 -0500

    more work on intersection.

--- analysis.py
+++ analysis.py
@@ -33,9 +33,9 @@ def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
     lineNum = 0
     for line in reader:
         if(lineNum > 1): #skip the headers
-            if(line[5] != str(HR[-1])):
+            if(float(line[5]) != HR[-1]):
                 HR.append(float(line[5]))
-            if(line[4] != str(systolic[-1])):
+            if(float(line[4]) != systolic[-1]):
                 systolic.append(float(line[4]))
 
         lineNum += 1
@@ -44,14 +44,33 @@ def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
     print(len(systolic))
     print(len(HR))
 
-    count = 0
+    runs = []
+    currentRun = []
+    start = -1
+    direction = 0
 
-    for i in range(0, len(HR)-1):
+    for i in range(0, len(systolic)-1):
         sysDiff = systolic[i+1] - systolic[i]
         HRDiff = HR[i+1] - HR[i]
 
-        if(sysDiff * HRDiff > 0): #moving in the same direction
-            print("same d")
+        if(sysDiff < 0 and HRDiff < 0): #decreasing
+            if(start != -1):
+                if(direction != 1): #then we've already started but it's in the increasing direction
+                                    #time to end the current run and start a new one
+                    start = i
+                    direction = -1
+                    currentRun.append(i)
+                    runs.append(currentRun)
+                    currentRun = []
+                else:
+                    currentRun.append(i)
+            else:
+                start = i
+                direction = -1
+                currentRun = [i]
+
+
+    print(runs)
 
 
 
