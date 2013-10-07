7d74bc7db608abaaa077fff87bb4d451e9b0d6d1
Andrew Lilja (Work) <alilja@iastate.edu>
2013-10-07 10:46:52 -0500

    Documentation + simple code to verify captured runs. Separated CSV analysis into different function.

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import csv
 
-def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
+#readCSVFile: str --> tuple-of-list-of-num
+def readCSVFile(fileName):
     f = open(fileName,"r")
     reader = csv.reader(f)
 
@@ -16,10 +17,28 @@ def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
                 systolic.append(float(line[4]))
 
         lineNum += 1
-    f.close()  
+    f.close()
 
-    print(len(systolic))
-    print(len(HR))
+    return (systolic, HR)  
+
+#findMatchingRuns: list-of-num, list-of-num, num, num --> list-of-list-of-num
+def findMatchingRuns(systolic, HR, clusterWidth = 3, lag = 1):
+    """Takes two lists of numbers and determines when they are both moving
+    in the same direction; that is, when they are both increasing at the same
+    time or both decreasing at the same time. Example:
+
+        a = [0,1,2, 3,4,5,1,0,9, 8]
+        b = [1,4,9,13,0,3,2,0,11,14]
+
+    The function will find the indices 0:3, 4:5, 5:7, and 7:8. It will also
+    determine the length and direction. Output is in the form of lists:
+
+        [runStart, runEnd, runLength, runDirection]
+
+    Direction is either +1 or -1.
+
+    clusterWidth is the minimum length of each run. lag is the difference
+    in offset between the second list and the first list."""
 
     runs = []
     count = 0
@@ -54,10 +73,27 @@ def baroAnalysis(fileName, clusterWidth = 3, lag = 1):
             if(count >= 0):
                 runs.append([i - count - 1, i - 1, count, direction])
                 count = 0
-        print(str(i)+": "+str(direction))
 
 
 
     return [r for r in runs if r[2] >= clusterWidth]
 
-print(baroAnalysis("testData.csv"))
+data = readCSVFile("testData.csv")
+
+
+runs = findMatchingRuns(data[0],data[1])
+print(runs)
+for run in runs:
+    runStart = run[0]
+    runEnd = run[1]
+    runLength = run[2]
+    runDirection = run[3]
+
+    for i in range(runStart - 3, runEnd + 3):
+        out = ""
+        if(i == runStart or i == runEnd):
+            out += "> "
+        out += str(data[0][i]) + "    " + str(data[1][i])
+        print(out)
+
+    print("\n\n")
