e2c98b2c63c2a61998d7525b3e4bcc1b08161510
Andrew Lilja (Work) <alilja@iastate.edu>
2013-10-08 14:04:12 -0500

    Added header-stripping capacity to CSV analysis. New datafile.

--- analysis.py
+++ analysis.py
@@ -1,28 +1,36 @@
 import csv
 
-#readCSVFile: str --> tuple-of-list-of-num
-def readCSVFile(fileName):
+#readCSVFile: str, int, int, int --> tuple-of-list-of-num
+def readCSVFile(fileName, headerLength = 1, HRChannel = 42, SBPChannel = 40):
     f = open(fileName,"r")
     reader = csv.reader(f)
 
     HR = [0]
-    systolic = [0]
+    SBP = [0]
+
+    HRIndex = 0
+    SBPIndex = 0
 
     lineNum = 0
     for line in reader:
-        if(lineNum > 1): #skip the headers
-            if(float(line[5]) != HR[-1]):
+        if("CH"+str(HRChannel) in line):
+            HRIndex = line.index("CH"+str(HRChannel))
+            SBPIndex = line.index("CH"+str(SBPChannel))
+            print("HR index: "+str(HRIndex))
+            print("SBPIndex: "+str(SBPIndex))
+        if(lineNum > headerLength): #skip the headers
+            if(float(line[HRIndex]) != HR[-1]):
                 HR.append(float(line[5]))
-            if(float(line[4]) != systolic[-1]):
-                systolic.append(float(line[4]))
+            if(float(line[SBPIndex]) != SBP[-1]):
+                SBP.append(float(line[4]))
 
         lineNum += 1
     f.close()
-
-    return (systolic, HR)  
+    print("Finished analyzing file \""+fileName+"\"")
+    return (SBP, HR)  
 
 #findMatchingRuns: list-of-num, list-of-num, num, num --> list-of-list-of-num
-def findMatchingRuns(systolic, HR, clusterWidth = 3, lag = 0):
+def findMatchingRuns(SBP, HR, clusterWidth = 3, lag = 0):
     """Takes two lists of numbers and determines when they are both moving
     in the same direction; that is, when they are both increasing at the same
     time or both decreasing at the same time. Example:
@@ -43,11 +51,11 @@ def findMatchingRuns(systolic, HR, clusterWidth = 3, lag = 0):
     runs = []
     count = 0
     direction = 0
-    for i in range(1, len(systolic)):
-        sysDiff = systolic[i] - systolic[i - 1]
+    for i in range(1, len(SBP)):
+        SBPDiff = SBP[i] - SBP[i - 1]
         HRDiff = HR[i + lag] - HR[i + lag - 1]
 
-        if(HRDiff > 0 and sysDiff > 0):
+        if(HRDiff > 0 and SBPDiff > 0):
             if(direction >= 0):
                 count += 1
                 direction = 1
@@ -56,7 +64,7 @@ def findMatchingRuns(systolic, HR, clusterWidth = 3, lag = 0):
                 count = 0
                 direction = 1
                 count = 1
-        elif(HRDiff < 0 and sysDiff < 0):
+        elif(HRDiff < 0 and SBPDiff < 0):
             if(direction <= 0):
                 count += 1
                 direction = -1
@@ -65,17 +73,16 @@ def findMatchingRuns(systolic, HR, clusterWidth = 3, lag = 0):
                 count = 0
                 direction = -1
                 count = 1
-        elif(HRDiff == 0 and sysDiff == 0):
+        elif(HRDiff == 0 and SBPDiff == 0):
             count +=1 
         else:
             runs.append([i - count - 1, i - 1, count, direction])
             count = 0
-
-
-
     return [r for r in runs if r[2] >= clusterWidth]
 
-data = readCSVFile("testData.csv")
+
+
+data = readCSVFile("testData.csv", 32)
 runs = findMatchingRuns(data[0],data[1], 2, 1)
 
 print(runs)
